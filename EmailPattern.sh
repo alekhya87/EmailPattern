4f350b7e867da8c75c3dde2858d7979a78e1259b
alekhya87 <davulurialekhya87909.com>
2020-10-14 12:04:16 +0530

    EmailUC-3 is done

--- EmailPattern.sh
+++ EmailPattern.sh
@@ -1,4 +1,12 @@
 #!/bin/bash/ -x
 echo " email validation"
-
+echo "enter the email address"
+read email
+emailPattern="^[a-zA-Z0-9]*@[a-zA-Z0-9]+.[a-zA-Z]{2,4}$"
+if [[ $email =~ $emailPattern ]]
+then
+	echo "valid email"
+else
+	echo "invalid email"
+fi
 
